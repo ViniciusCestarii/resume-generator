7bf464a5fe0cd1f46f180bcfbf6cc934c45b503e
ViniciusCestarii <viniciuscestari01@gmail.com>
2025-01-29 16:11:12 -0300

    fix: wrong theme variable (0.25) and return resume-rose.css

--- src/components/rose/header.tsx
+++ src/components/rose/header.tsx
@@ -9,7 +9,7 @@ function Header({ name, contact }: HeaderProps) {
   return (
     <header className="px-rose-padding pt-rose-padding bg-rose-300/50 h-64">
       <h1 className="text-4xl font-bold">{name}</h1>
-      <div className="bg-[#333a30] h-0.25 w-10" />
+      <div className="bg-[#333a30] h-025 w-10" />
       <ul className="space-y-3 pt-3">
         <li className="flex gap-2 text-sm">
           <div className="size-6 rounded-full p-1 bg-white"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect width="20" height="16" x="2" y="4" rx="2" /><path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7" /></svg></div>
